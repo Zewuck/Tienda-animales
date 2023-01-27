360e9f7e5c6e20523f364a63599fbb1a8eb9541b
Joel <joelochoa.s023@gmail.com>
2023-01-27 09:45:06 -0500

    agregue el bloque despues del gato

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -6,14 +6,27 @@ export function Inicio() {
     <>
       <div className="background">
         <h1 className="title">Cat's Shop</h1>
-        <p>Elige la comida para perro o gato favorita y en menos de tres horas estará en la puerta de tu casa, también puedes programar la entrega en el horario de tu elección.</p>
-        <button><Link to={"/acerca"}>IR A LA TIENDA</Link></button>
+        <p>
+          Elige la comida para perro o gato favorita y en menos de tres horas
+          estará en la puerta de tu casa, también puedes programar la entrega en
+          el horario de tu elección.
+        </p>
+        <button>
+          <Link to={"/tienda"}>IR A LA TIENDA</Link>
+        </button>
         <div className="gato-container">
           <img src={require("../imagen-gato.png")} className="gato" alt="" />
-            <img src={require("../wave.png")} className="fondo" alt="" />
+          <img src={require("../wave.png")} className="fondo" alt="" />
         </div>
       </div>
-      
+      <div>
+        <h2>Lorem ipsum dolor sit no </h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
+          repudiandae suscipit libero, quaerat officiis nisi enim iste, soluta
+        </p>
+        <button><Link to={"/acerca"}>CONOCE MAS SOBRE NOSTROS</Link></button>
+      </div>
       <div className="next"> KillYourself</div>
     </>
   );
