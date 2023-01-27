8247dd1e0d9c3d3dc74435481ff460af71a5d70c
zewuck <deyner.higuera.26@gmail.com>
2023-01-26 19:20:39 -0500

    Se mejoro el estilo-Gato

--- src/FondoInicio.jsx
+++ src/FondoInicio.jsx
@@ -6,11 +6,11 @@ export function FondoInicio() {
       <div className="background">
         <div className="gato-container">
           <img src={require("./imagen-gato.png")} className="gato" alt="" />
-          <div>
             <img src={require("./wave.png")} className="fondo" alt="" />
-          </div>
         </div>
       </div>
+      
+      <div className="next"> KillYourself</div>
     </>
   );
 }
