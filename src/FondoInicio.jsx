bd4494eda6f81020b862637eca60b370d0b9cef0
zewuck <deyner.higuera.26@gmail.com>
2023-01-27 08:43:19 -0500

    Actualizacion fondo terminado

--- src/FondoInicio.jsx
+++ src/FondoInicio.jsx
@@ -4,6 +4,7 @@ export function FondoInicio() {
   return (
     <>
       <div className="background">
+        <h1 className="title">Cat's Shop</h1>
         <div className="gato-container">
           <img src={require("./imagen-gato.png")} className="gato" alt="" />
             <img src={require("./wave.png")} className="fondo" alt="" />
