0dbe4f9ea80cacf2b90eef0cecd078ae526ddcc9
zewuck <deyner.higuera.26@gmail.com>
2023-02-02 12:38:39 -0500

    se termino la pagina about us

--- src/assets/imagenes.js
+++ src/assets/imagenes.js
@@ -4,5 +4,8 @@ import wave from "./img/wave-pink.png";
 import comida from "./img/comida.avif";
 import arrow from "./img/arrow.png"
 import pink from "./img/title-plink.png"
+import conejo from "./img/conejo.png"
+import perro from "./img/perro.png"
+import perro2 from "./img/perro2.png"
 
-export const img = { gato, logo, wave, comida, arrow, pink };
+export const img = { gato, logo, wave, comida, arrow, pink, conejo, perro, perro2 };
