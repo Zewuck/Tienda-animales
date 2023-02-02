42cb9da53116663c9676b8945197635d115dc059
Joel <joelochoa.s023@gmail.com>
2023-02-02 12:47:52 -0500

    se agrego orden a las imagenes xd

--- src/assets/imagenes.js
+++ src/assets/imagenes.js
@@ -2,23 +2,17 @@ import gato from "./img/imagen-gato.png";
 import logo from "./img/logo.png";
 import wave from "./img/wave-pink.png";
 import comida from "./img/comida.avif";
-<<<<<<< HEAD
-import arrow from "./img/arrow.png"
-import pink from "./img/title-plink.png"
-import conejo from "./img/conejo.png"
-import perro from "./img/perro.png"
-import perro2 from "./img/perro2.png"
-
-export const img = { gato, logo, wave, comida, arrow, pink, conejo, perro, perro2 };
-=======
 import arrow from "./img/arrow.png";
 import pink from "./img/title-plink.png";
 import concentrado from "./img/concentrado.jpg";
 import caja from "./img/caja-de-entrega.png";
 import camion from "./img/camion.png";
 import petfood from "./img/comida-para-gatos.jpg";
-import gatotel from './img/gato-1.avif';
-import gatolente from './img/gato-2.jpeg';
+import gatotel from "./img/gato-1.avif";
+import gatolente from "./img/gato-2.jpeg";
+import conejo from "./img/conejo.png";
+import perro from "./img/perro.png";
+import perro2 from "./img/perro2.png";
 
 export const img = {
   gato,
@@ -32,6 +26,8 @@ export const img = {
   camion,
   petfood,
   gatotel,
-  gatolente
+  gatolente,
+  conejo,
+  perro,
+  perro2,
 };
->>>>>>> 6c1b50b7cb38882c1ccb05a1aadc888c7aa6c764
