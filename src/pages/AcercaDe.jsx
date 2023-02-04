532d2a3d5f4d5de2ef8df4b29c8fc06320f1079e
zewuck <deyner.higuera.26@gmail.com>
2023-02-04 12:06:27 -0500

    Contactanos terminado

--- src/pages/AcercaDe.jsx
+++ src/pages/AcercaDe.jsx
@@ -200,50 +200,6 @@ export default function AcercaDe() {
           </div>
         </div>
       </div>
-
-      {/* <div>
-      <div>
-        <h3>Sobre nosotros</h3>
-        <h1>Helping businesses succeed through the power of video.</h1>
-        <p>
-          Video is the future of business in this digital-focused world. Vidyard
-          uses video to change the way companies connect and communicate. We
-          help organizations of all sizes humanize their communications and
-          personalize their customer experiences
-        </p>
-        <img src="" alt="" />
-      </div>
-      <div>
-        <h2>
-          Record and send videos to reach prospects, connect with customers, and
-          change the way you work
-        </h2>
-        <p>
-          Vidyard is built for business. Our platform goes beyond just video
-          hosting and management. Connect with viewers through personalized
-          video experiences. Explore analytical insights about your audience.
-          Turn those insights into action through integrations with top
-          enterprise tools. Prove the impact of your video programs. Global
-          leaders and industry pioneers on the Fortune 500 list and beyond rely
-          on Vidyard to power their video strategies and turn viewers into
-          customers.
-        </p>
-      </div>
-      {
-        
-      }
-      <div>
-        <img src="" alt="" />
-      </div>
-      <div>
-        <h3>OUR HISTORY</h3>
-        <h2>The Story of Vidyard</h2>
-        <p>After meeting at the University of Waterloo, co-founders Michael Litt and Devon Galloway formed an innovative video startup that would ultimately graduate from the elite Y Combinator program. Since then, Vidyard has grown to include competitive features, integrations, and thought leadership that position us as a top video business platform.</p>
-        <img src="" alt="" />
-        <h2>Redwoods Media Becomes Vidyard</h2>
-        <p>Michael Litt and Devon Galloway found Redwoods Media, the first video production company to offer video hosting, analytics, and performance guarantees. Envisioning the future needs of customers, the potential of video, and the power of analytics, Redwoods Media later becomes Vidyard to focus exclusively on video sales and marketing technology.</p>
-      </div>
-    </div> */}
     </>
   );
 }
--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,10 +1,54 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-
+import "../contacto.css";
+import { img } from "../assets/imagenes.js";
 
 export default function Contacto() {
   return (
     <>
+    <div className="contact1">
+		<div className="container-contact1">
+			<div className="contact1-pic js-tilt" data-tilt>
+				<img src={img.contacto} className="imagen-contacto" alt="IMG"/>
+			</div>
+
+			<form className="contact1-form validate-form">
+				<span className="contact1-form-title">
+					Get in touch
+				</span>
+
+				<div className="wrap-input1 validate-input" data-validate = "Name is required">
+					<input className="input1" type="text" name="name" placeholder="Name"/>
+					<span className="shadow-input1"></span>
+				</div>
+
+				<div className="wrap-input1 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<input className="input1" type="text" name="email" placeholder="Email"/>
+					<span className="shadow-input1"></span>
+				</div>
+
+				<div className="wrap-input1 validate-input" data-validate = "Subject is required">
+					<input className="input1" type="text" name="subject" placeholder="Subject"/>
+					<span className="shadow-input1"></span>
+				</div>
+
+				<div className="wrap-input1 validate-input" data-validate = "Message is required">
+					<textarea className="input1" name="message" placeholder="Message"></textarea>
+					<span className="shadow-input1"></span>
+				</div>
+
+				<div className="container-contact1-form-btn button">
+					<button className="contact1-form-btn button">
+						<span>
+							Send Email
+							<i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+						</span>
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
+      
       <Formik
         initialValues={{
           nombre: "",
@@ -21,47 +65,59 @@ export default function Contacto() {
 
           if (!valores.correo) {
             errores.correo = "Porfavor ingrese un correo electronico";
-          } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.correo)) {
-            errores.correo = "El correo solo puede contener letras, numeros, puntos, guiones y guion bajo ";
+          } else if (
+            !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+              valores.correo
+            )
+          ) {
+            errores.correo =
+              "El correo solo puede contener letras, numeros, puntos, guiones y guion bajo ";
           }
 
           if (!valores.mensaje) {
             errores.mensaje = "Porfavor ingrese un mensaje";
           } else if (!/^[\s\S]{0,400}$/.test(valores.mensaje)) {
-            errores.mensaje = "el mensaje solo puede tener maximo 400 caracteres ";
-           }
+            errores.mensaje =
+              "el mensaje solo puede tener maximo 400 caracteres ";
+          }
           return errores;
         }}
-        onSubmit={(valores, {resetForm}) => {
+        onSubmit={(valores, { resetForm }) => {
           resetForm();
           console.log(valores);
         }}
       >
-        {({errors,handleSubmit, isSubmitting }) => (
+        {({ errors, handleSubmit, isSubmitting }) => (
           <Form onSubmit={handleSubmit}>
             <div>
               <label htmlFor="nombre">Nombre</label>
-              <Field
-                type="text"
+              <Field type="text" name="nombre" id="nombre" />
+              <ErrorMessage
                 name="nombre"
-                id="nombre"
+                component={() => <div>{errors.nombre}</div>}
               />
-              <ErrorMessage name="nombre" component={()=>(<div>{errors.nombre}</div>)}/>
             </div>
             <div>
               <label htmlFor="correo">Correo</label>
-              <Field
-                type="email"
+              <Field type="email" name="correo" id="correo" />
+              <ErrorMessage
                 name="correo"
-                id="correo"
+                component={() => <div>{errors.correo}</div>}
               />
-              <ErrorMessage name="correo" component={()=>(<div>{errors.correo}</div>)}/>
             </div>
             <div>
               <label htmlFor="mensaje">Mensaje</label>
-              <Field name="mensaje" as="textarea" id="mensaje" rows="5" cols="30"></Field>
-              <ErrorMessage name="mensaje" component={()=>(<div>{errors.mensaje}</div>)}/>
-
+              <Field
+                name="mensaje"
+                as="textarea"
+                id="mensaje"
+                rows="5"
+                cols="30"
+              ></Field>
+              <ErrorMessage
+                name="mensaje"
+                component={() => <div>{errors.mensaje}</div>}
+              />
             </div>
             <button type="submit" disabled={isSubmitting}>
               {isSubmitting ? "Saving..." : "Save"}
