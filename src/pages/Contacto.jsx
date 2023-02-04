66368032e3ac9183dcb32605f764c60f38a26e37
Joel <joelochoa.s023@gmail.com>
2023-02-04 12:38:45 -0500

    se agregaron los estilos al formulario funcional

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -6,125 +6,140 @@ import { img } from "../assets/imagenes.js";
 export default function Contacto() {
   return (
     <>
-    <div className="contact1">
-		<div className="container-contact1">
-			<div className="contact1-pic js-tilt" data-tilt>
-				<img src={img.contacto} className="imagen-contacto" alt="IMG"/>
-			</div>
 
-			<form className="contact1-form validate-form">
-				<span className="contact1-form-title">
-					Get in touch
-				</span>
+      {/* FORMIK */}
+      <div className="contact1">
+        <div className="container-contact1">
+          <div className="contact1-pic js-tilt" data-tilt>
+            <img src={img.contacto} className="imagen-contacto" alt="IMG" />
+          </div>
+          <Formik
+            initialValues={{
+              nombre: "",
+              correo: "",
+              subject: "",
+              mensaje: "",
+            }}
+            validate={(valores) => {
+              const errores = {};
+              if (!valores.nombre) {
+                errores.nombre = "Porfavor ingrese un nombre";
+              } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)) {
+                errores.nombre =
+                  "El nombre solo puede contener letras y espacios ";
+              }
 
-				<div className="wrap-input1 validate-input" data-validate = "Name is required">
-					<input className="input1" type="text" name="name" placeholder="Name"/>
-					<span className="shadow-input1"></span>
-				</div>
+              if (!valores.correo) {
+                errores.correo = "Porfavor ingrese un correo electronico";
+              } else if (
+                !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+                  valores.correo
+                )
+              ) {
+                errores.correo =
+                  "El correo solo puede contener letras, numeros, puntos, guiones y guion bajo ";
+              }
+              if (!valores.subject) {
+                errores.subject = "Porfavor ingrese un asunto";
+              } else if (!/^[\s\S]{0,50}$/.test(valores.subject)) {
+                errores.subject =
+                  "El asunto solo puede tener maximo 50 caracteres ";
+              }
 
-				<div className="wrap-input1 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-					<input className="input1" type="text" name="email" placeholder="Email"/>
-					<span className="shadow-input1"></span>
-				</div>
+              if (!valores.mensaje) {
+                errores.mensaje = "Porfavor ingrese un mensaje";
+              } else if (!/^[\s\S]{0,400}$/.test(valores.mensaje)) {
+                errores.mensaje =
+                  "El mensaje solo puede tener maximo 400 caracteres ";
+              }
+              return errores;
+            }}
+            onSubmit={(valores, { resetForm }) => {
+              resetForm();
+              console.log(valores);
+            }}
+          >
+            {({ errors, handleSubmit, isSubmitting }) => (
+              <Form
+                onSubmit={handleSubmit}
+                className="contact1-form validate-form"
+              >
+                <span className="contact1-form-title">Get in touch</span>
 
-				<div className="wrap-input1 validate-input" data-validate = "Subject is required">
-					<input className="input1" type="text" name="subject" placeholder="Subject"/>
-					<span className="shadow-input1"></span>
-				</div>
+                <div className="wrap-input1 validate-input">
+                  <Field
+                    className="input1"
+                    type="text"
+                    name="nombre"
+                    id="nombre"
+                    placeholder="Name"
+                  />
+                  <span className="shadow-input1"></span>
+                  <ErrorMessage
+                    name="nombre"
+                    component={() => <div>{errors.nombre}</div>}
+                  />
+                </div>
+                <div className="wrap-input1 validate-input">
+                  <Field
+                    className="input1"
+                    type="email"
+                    name="correo"
+                    id="correo"
+                    placeholder="Email"
+                  />
+                  <ErrorMessage
+                    name="correo"
+                    component={() => <div>{errors.correo}</div>}
+                  />
+                </div>
+                <div className="wrap-input1 validate-input">
+                  <Field
+                    className="input1"
+                    type="text"
+                    name="subject"
+                    id="subject"
+                    placeholder="Subject"
+                  />
+                  <ErrorMessage
+                    name="subject"
+                    component={() => <div>{errors.subject}</div>}
+                  />
+                </div>
+                <div className="wrap-input1 validate-input">
+                  <Field
+                    className="input1"
+                    name="mensaje"
+                    as="textarea"
+                    id="mensaje"
+                    rows="5"
+                    cols="30"
+                    placeholder="Message"
+                  ></Field>
+                  <ErrorMessage
+                    name="mensaje"
+                    component={() => <div>{errors.mensaje}</div>}
+                  />
+                </div>
 
-				<div className="wrap-input1 validate-input" data-validate = "Message is required">
-					<textarea className="input1" name="message" placeholder="Message"></textarea>
-					<span className="shadow-input1"></span>
-				</div>
+                <div className="container-contact1-form-btn button">
+                  <button className="contact1-form-btn button">
+                    <span>
+                      Send Email
+                      <i
+                        className="fa fa-long-arrow-right"
+                        aria-hidden="true"
+                      ></i>
+                    </span>
+                  </button>
+                </div>
 
-				<div className="container-contact1-form-btn button">
-					<button className="contact1-form-btn button">
-						<span>
-							Send Email
-							<i className="fa fa-long-arrow-right" aria-hidden="true"></i>
-						</span>
-					</button>
-				</div>
-			</form>
-		</div>
-	</div>
-      
-      <Formik
-        initialValues={{
-          nombre: "",
-          correo: "",
-          mensaje: "",
-        }}
-        validate={(valores) => {
-          const errores = {};
-          if (!valores.nombre) {
-            errores.nombre = "Porfavor ingrese un nombre";
-          } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)) {
-            errores.nombre = "El nombre solo puede contener letras y espacios ";
-          }
-
-          if (!valores.correo) {
-            errores.correo = "Porfavor ingrese un correo electronico";
-          } else if (
-            !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
-              valores.correo
-            )
-          ) {
-            errores.correo =
-              "El correo solo puede contener letras, numeros, puntos, guiones y guion bajo ";
-          }
-
-          if (!valores.mensaje) {
-            errores.mensaje = "Porfavor ingrese un mensaje";
-          } else if (!/^[\s\S]{0,400}$/.test(valores.mensaje)) {
-            errores.mensaje =
-              "el mensaje solo puede tener maximo 400 caracteres ";
-          }
-          return errores;
-        }}
-        onSubmit={(valores, { resetForm }) => {
-          resetForm();
-          console.log(valores);
-        }}
-      >
-        {({ errors, handleSubmit, isSubmitting }) => (
-          <Form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="nombre">Nombre</label>
-              <Field type="text" name="nombre" id="nombre" />
-              <ErrorMessage
-                name="nombre"
-                component={() => <div>{errors.nombre}</div>}
-              />
-            </div>
-            <div>
-              <label htmlFor="correo">Correo</label>
-              <Field type="email" name="correo" id="correo" />
-              <ErrorMessage
-                name="correo"
-                component={() => <div>{errors.correo}</div>}
-              />
-            </div>
-            <div>
-              <label htmlFor="mensaje">Mensaje</label>
-              <Field
-                name="mensaje"
-                as="textarea"
-                id="mensaje"
-                rows="5"
-                cols="30"
-              ></Field>
-              <ErrorMessage
-                name="mensaje"
-                component={() => <div>{errors.mensaje}</div>}
-              />
-            </div>
-            <button type="submit" disabled={isSubmitting}>
-              {isSubmitting ? "Saving..." : "Save"}
-            </button>
-          </Form>
-        )}
-      </Formik>
+                {/*className="contact1-form-btn button" */}
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
     </>
   );
 }
